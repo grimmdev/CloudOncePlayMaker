a4063c9c29581de6ee151aa649680de95782cc72
Sean Loper <grimmdev@gmail.com>
2015-06-17 22:25:40 -0400

    Did we login? let's check.. and if so move on..

---
+++ PlayMaker/Actions/CloudOnce/CheckLogin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Trollpants.CloudOnce;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("CloudOnce")]
+	[Tooltip("Check the login state, if logged in then continue.")]
+	public class CheckLogin : FsmStateAction
+	{
+		
+		[Tooltip("Event to send if the variable changes.")]
+		public FsmEvent changedEvent;
+		
+		bool previousValue;
+		
+		public override void Reset()
+		{
+			changedEvent = null;
+		}
+		
+		public override void OnEnter()
+		{
+
+		}
+		
+		public override void OnUpdate()
+		{
+			if (Cloud.Provider.IsSignedIn)
+				Fsm.Event (changedEvent);
+		}
+	}
+}
+
